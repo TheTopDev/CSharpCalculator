f4d9024d852d487175705bae3a867bc092ca598f
Lucas Garofalo <garofalodev@gmail.com>
2021-02-21 17:12:20 -0600

    First Commit

---
+++ CSharpM1P2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpM1P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number between 1-255: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter a number between 1-255: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
